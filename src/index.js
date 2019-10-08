28db10ed6c4704aba53c8af5282e20b07ad52816
Junior Dugue <jrdugue@gmail.com>
2019-10-07 21:22:15 -0400

    updated with latest notes/changes
    updated with latest notes/changes

--- src/index.js
+++ src/index.js
@@ -2,30 +2,31 @@ import ReactDOM from "react-dom";
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import axios from "axios";
-import {useInput, useLocalStorage} from "./utils/input";
+import { useInput, useLocalStorage } from "./utils/input";
 
 function App(props) {
   // const [breed, setBreed] = useInput("husky");
-  const [breed, setBreed] = useLocalStorage("breed","husky");
+  const [breed, setBreed] = useLocalStorage("breed", "husky");
   // const [breed, setBreed] = useState("husky");
   const [images, setImages] = useState([]);
 
   //custom hook to count images
-  const [count, setCount] = useState(["count", 1]);
+  const [count, setCount] = useLocalStorage(["count", 1]);
 
   useEffect(() => {
     setImages([]);
     fetchDogImages();
-  }, [breed]);
+  }, [breed, count]);
 
-  const handleChange = e => {
-    setBreed(e.target.value);
-  };
+  //
+  // const handleChange = e => {
+  //   setBreed(e.target.value);
+  // };
 
   // extract this function from lifecycle event since it's used multiple times
   const fetchDogImages = () => {
     axios
-      .get(`https://dog.ceo/api/breed/${breed}/images`)
+      .get(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
       .then(res => {
         setImages(res.data.message);
       })
@@ -38,13 +39,19 @@ function App(props) {
     <>
       <h1>The Dog Website</h1>
 
-      <select value={breed} onChange={handleChange}>
+      <select value={breed} onChange={e => setBreed(e.target.value)}>
         <option value="husky">Husky</option>
         <option value="beagle">Beagle</option>
         <option value="corgi">Corgi</option>
         <option value="boxer">Boxer</option>
       </select>
 
+      <input
+        type="number"
+        placeholder="Image count"
+        value={count}
+        onChange={e => setCount(e.target.value)} //
+      />
       <div>
         {images.map((image, index) => (
           <img key={index} src={image} alt="Dog" />
